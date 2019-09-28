96496eb839721ddf9837bd1c290bc624149015d1
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-28 16:22:32 -0700

    Add files via upload

---
+++ bases.h
@@ -0,0 +1,9 @@
+// bases.h
+#include "baseChange.cpp"
+#include "add.cpp"
+#include "divide.cpp"
+#include "subtract.cpp"
+#include "euclideanAlgorithm.cpp"
+#include "remainder.cpp"
+#include "multiply.cpp"
+#include<string> // for string class
